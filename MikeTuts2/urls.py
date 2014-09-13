8f1cd7ee43d818a842631913f5806c98d1300d94
Suchet Bargoti <bargoti.suchet@gmail.com>
2014-09-13 15:51:12 +1000

    Finished tutorial 4, added article to html

--- MikeTuts2/urls.py
+++ MikeTuts2/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from article.views import HelloTemplate
+# from article.views import HelloTemplate
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -7,11 +7,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^hello/$', 'article.views.hello'),
-    url(r'^hello_template/$', 'article.views.hello_template'),
-    url(r'^hello_template_simple/$', 'article.views.hello_template_simple'),
-    url(r'^hello_class_view/$', HelloTemplate.as_view()),
-
+    # url(r'^hello/$', 'article.views.hello'),
+    # url(r'^hello_template/$', 'article.views.hello_template'),
+    # url(r'^hello_template_simple/$', 'article.views.hello_template_simple'),
+    # url(r'^hello_class_view/$', HelloTemplate.as_view()),
+    (r'^articles/', include('article.urls')),
     # url(r'^$', 'MikeTuts2.views.home', name='home'),
     # url(r'^MikeTuts2/', include('MikeTuts2.foo.urls')),
 
@@ -19,5 +19,5 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
